8e94379dd65c3472cc6c7d651cdb1576ee39e2b6
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-11-02 14:44:49 -0700

    added general stucture to ArticleCard

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -1,7 +1,29 @@
 import "./ArticleCard.css";
 
-const ArticleCard = () => {
-  return <div></div>;
+const ArticleCard = ({ article, size }) => {
+  return (
+    <div className={size === "main" ? "mainArticle" : "secondaryArticle"}>
+      <div>
+        <p>
+          <a href={article.author.profile}>{article.author.name}</a> wrote
+        </p>
+      </div>
+      <h1>{article.title}</h1>
+      <h2>
+        {article.date} in{" "}
+        {article.tags.map((tag) => {
+          return (
+            <p>
+              <a href={tag.page} className="tagLink">
+                tag.name
+              </a>
+              ,{" "}
+            </p>
+          );
+        })}
+      </h2>
+    </div>
+  );
 };
 
 export default ArticleCard;
