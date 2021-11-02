93f4348ff2739fb65176b6bfff920b13ad3c898b
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-11-02 16:59:16 -0700

    added content to footer

--- src/components/PageFooter/PageFooter.js
+++ src/components/PageFooter/PageFooter.js
@@ -1,5 +1,196 @@
 const PageFooter = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <h1>Brows All Smashing Magazine Topics</h1>
+      <ul>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/accessibility">
+            Accessibility
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/android">
+            Android
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/animation">
+            Animation
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/apps">Apps</a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/css">CSS</a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/design">Design</a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/design-patterns">
+            Design Patterns
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/e-commerce">
+            E-Commerce
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/freebies">
+            Freebies
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/graphics">
+            Graphics
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/html">HTML</a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/illustrator">
+            Illustrator
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/inspiration">
+            Inspiration
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/ios">iOS</a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/javascript">
+            JavaScript
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/mobile">Mobile</a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/pattern-libraries">
+            Pattern Libraries
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/performance">
+            Performance
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/photoshop">
+            Photoshop
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/plugins">
+            Plugins
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/react">React</a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/responsive-design">
+            Responsive Design
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/service-workers">
+            Service Workers
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/sketch">Sketch</a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/typography">
+            Typography
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/ui">UI</a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/usability">
+            Usability
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/user-experience">
+            User Experience
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/wallpapers">
+            Wallpapers
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/web-design">
+            Web Design
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/wordpress">
+            WordPress
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/category/workflow">
+            Workflow
+          </a>
+        </li>
+      </ul>
+      <div>
+        <p>With a commitment to quality content for the design community</p>
+        <p>Founded by Vitaly Friedman and Sven Lennartz. 2006-2021.</p>
+        <p>
+          Smashing is proudly runnig on{" "}
+          <a href="https://www.netlify.com/?utm_source=link&utm_medium=parter&utm_campaign=sm-footer">
+            Netlify
+          </a>
+          .
+        </p>
+        <p>
+          Fonts by <a href="http://latinotype.com/">Latinotype</a>.
+        </p>
+      </div>
+      <ul>
+        <li>
+          <a href="https://www.smashingmagazine.com/write-for-us/">
+            Write for us
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/contact/">Contact us</a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/about/">About us</a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/privacy-policy/">
+            Privacy Policy
+          </a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/auth/">Membership login</a>
+        </li>
+        <li>
+          <a href="https://www.smashingmagazine.com/delivery-times/">
+            Delivery Times
+          </a>
+        </li>
+        <li>
+          <a href="advertising@smashingmagazine.com">Advertise</a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default PageFooter;
