e0b2a2974b9b07a4cab4edaf7cf9610ee60c5899
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-11-02 15:49:47 -0700

    added placeholder content to GuideCard

--- src/components/GuideCard/GuideCard.js
+++ src/components/GuideCard/GuideCard.js
@@ -1,5 +1,15 @@
-const GuideCard = () => {
-  return <div></div>;
+import "./GuideCard.css";
+
+const GuideCard = ({ title, desc, link, linkText }) => {
+  return (
+    <div className="guideCard">
+      <h2>{title}</h2>
+      <p>{desc}</p>
+      <a href={link}>
+        <button>{linkText}</button>
+      </a>
+    </div>
+  );
 };
 
 export default GuideCard;
