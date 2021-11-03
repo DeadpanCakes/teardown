bd125c6dc9c815c34f670104d585b0661cc65bee
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-11-02 18:04:50 -0700

    sized article cards

--- src/components/ArticleCard/ArticleCard.js
+++ src/components/ArticleCard/ArticleCard.js
@@ -2,26 +2,33 @@ import "./ArticleCard.css";
 
 const ArticleCard = ({ article, size }) => {
   return (
-    <div className={size === "main" ? "mainArticle" : "secondaryArticle"}>
+    <div
+      className={
+        size === "main" ? "mainArticle article" : "secondaryArticle article"
+      }
+    >
+      <img alt={article.author.name} />
       <div>
         <p>
           <a href={article.author.profile}>{article.author.name}</a> wrote
         </p>
+        <h1>{article.title}</h1>
+        <h2>
+          {article.date} in{" "}
+          {article.tags.map((tag, index) => {
+            return (
+              <p>
+                <a href={tag.page} className="tagLink">
+                  {tag.name}
+                </a>
+                {index + 1 < article.tags.length ? ", " : ""}
+              </p>
+            );
+          })}
+        </h2>
       </div>
-      <h1>{article.title}</h1>
-      <h2>
-        {article.date} in{" "}
-        {article.tags.map((tag, index) => {
-          return (
-            <p>
-              <a href={tag.page} className="tagLink">
-                {tag.name}
-              </a>
-              {index + 1 < article.tags.length ? ", " : ""}
-            </p>
-          );
-        })}
-      </h2>
+      <p className="articleSummary">{article.summary}</p>
+      <a href={article.link}>Continue Reading</a>
     </div>
   );
 };
