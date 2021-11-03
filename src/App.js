800ab74f7f010305dd846c580b747cdf9b170cb3
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-11-02 17:42:58 -0700

    grouped content and made display grid

--- src/App.js
+++ src/App.js
@@ -9,97 +9,106 @@ function App() {
   return (
     <div className="App">
       <PageHeader />
-      <ArticleCard
-        article={{
-          author: {
-            name: "Cosima Mielke",
-            profile: "https://www.smashingmagazine.com/author/cosima-mielke/",
-          },
-          date: "OCT 31, 2021",
-          title:
-            "A Splash Of Color For Misty Days (November 2021 Desktop Wallpapers Edition)",
-          tags: [
-            {
-              name: "Wallpapers",
-              tagLink: "https://www.smashingmagazine.com/categories/wallpapers",
-            },
-          ],
-        }}
-        size="main"
-      />
-      <ArticleCard
-        article={{
-          author: {
-            name: "Freya Giles",
-            profile: "https://www.smashingmagazine.com/author/freya-giles/",
-          },
-          date: "OCT 31, 2021",
-          title:
-            "50 Resources And Tools To Turbocharge Your Copywriting Skills",
-          tags: [
-            {
-              name: "Tools",
-              tagLink: "https://www.smashingmagazine.com/categories/tools",
-            },
-            {
-              name: "Copywriting",
-              tagLink:
-                "https://www.smashingmagazine.com/categories/copywriting",
-            },
-            {
-              name: "Content Strategy",
-              tagLink:
-                "https://www.smashingmagazine.com/categories/content-strategy",
-            },
-          ],
-        }}
-        size="secondary"
-      />
-      <ArticleCard
-        article={{
-          author: {
-            name: "Robert Sfichi",
-            profile: "https://www.smashingmagazine.com/author/robert-sfichi/",
-          },
-          date: "OCT 31, 2021",
-          title: "How To Build An Amazon Product Scraper With Node.js",
-          tags: [
-            {
-              name: "Node.js",
-              tagLink: "https://www.smashingmagazine.com/categories/node.js",
-            },
-            {
-              name: "JavaScript",
-              tagLink: "https://www.smashingmagazine.com/categories/javascript",
-            },
-            {
-              name: "Tools",
-              tagLink: "https://www.smashingmagazine.com/categories/tools",
-            },
-          ],
-        }}
-        size="secondary"
-      />
-      <Newsletter />
-      <div>
-        <GuideCard
-          title="Accessible Front-End Components"
-          link="https://www.smashingmagazine.com/2021/03/complete-guide-accessible-front-end-components/"
-          linkText="Explore Accessible Components"
-          desc="Explore Accessible Components"
-        />
-        <GuideCard
-          title="CSS Generators & Tools"
-          desc="Useful CSS tools for animations, shadows, cubic-bezier curves, easing gradients, filters, overlays, type scales"
-          link="https://www.smashingmagazine.com/2021/03/css-generators/"
-          linkText="Explore CSS Generators"
-        />
-        <GuideCard
-          title="Front-End Boilerplates & Starter Kits"
-          desc="HTML boilerplates, CSS Resets, forms, dev themes, gitignore, CSS snippets, static sites, style guides"
-          link="https://www.smashingmagazine.com/2021/06/useful-frontend-boilerplates-starter-kits/"
-          linkText="Explore Front-End Boilerplates"
-        />
+      <div className="content">
+        <main>
+          <ArticleCard
+            article={{
+              author: {
+                name: "Cosima Mielke",
+                profile:
+                  "https://www.smashingmagazine.com/author/cosima-mielke/",
+              },
+              date: "OCT 31, 2021",
+              title:
+                "A Splash Of Color For Misty Days (November 2021 Desktop Wallpapers Edition)",
+              tags: [
+                {
+                  name: "Wallpapers",
+                  tagLink:
+                    "https://www.smashingmagazine.com/categories/wallpapers",
+                },
+              ],
+            }}
+            size="main"
+          />
+          <ArticleCard
+            article={{
+              author: {
+                name: "Freya Giles",
+                profile: "https://www.smashingmagazine.com/author/freya-giles/",
+              },
+              date: "OCT 31, 2021",
+              title:
+                "50 Resources And Tools To Turbocharge Your Copywriting Skills",
+              tags: [
+                {
+                  name: "Tools",
+                  tagLink: "https://www.smashingmagazine.com/categories/tools",
+                },
+                {
+                  name: "Copywriting",
+                  tagLink:
+                    "https://www.smashingmagazine.com/categories/copywriting",
+                },
+                {
+                  name: "Content Strategy",
+                  tagLink:
+                    "https://www.smashingmagazine.com/categories/content-strategy",
+                },
+              ],
+            }}
+            size="secondary"
+          />
+          <ArticleCard
+            article={{
+              author: {
+                name: "Robert Sfichi",
+                profile:
+                  "https://www.smashingmagazine.com/author/robert-sfichi/",
+              },
+              date: "OCT 31, 2021",
+              title: "How To Build An Amazon Product Scraper With Node.js",
+              tags: [
+                {
+                  name: "Node.js",
+                  tagLink:
+                    "https://www.smashingmagazine.com/categories/node.js",
+                },
+                {
+                  name: "JavaScript",
+                  tagLink:
+                    "https://www.smashingmagazine.com/categories/javascript",
+                },
+                {
+                  name: "Tools",
+                  tagLink: "https://www.smashingmagazine.com/categories/tools",
+                },
+              ],
+            }}
+            size="secondary"
+          />
+        </main>
+        <Newsletter />
+        <div className="guides">
+          <GuideCard
+            title="Accessible Front-End Components"
+            link="https://www.smashingmagazine.com/2021/03/complete-guide-accessible-front-end-components/"
+            linkText="Explore Accessible Components"
+            desc="Explore Accessible Components"
+          />
+          <GuideCard
+            title="CSS Generators & Tools"
+            desc="Useful CSS tools for animations, shadows, cubic-bezier curves, easing gradients, filters, overlays, type scales"
+            link="https://www.smashingmagazine.com/2021/03/css-generators/"
+            linkText="Explore CSS Generators"
+          />
+          <GuideCard
+            title="Front-End Boilerplates & Starter Kits"
+            desc="HTML boilerplates, CSS Resets, forms, dev themes, gitignore, CSS snippets, static sites, style guides"
+            link="https://www.smashingmagazine.com/2021/06/useful-frontend-boilerplates-starter-kits/"
+            linkText="Explore Front-End Boilerplates"
+          />
+        </div>
       </div>
       <PageFooter />
     </div>
